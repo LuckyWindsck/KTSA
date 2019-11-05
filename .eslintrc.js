58a8c410ed8a7fd78f9a2452c40e44ac8ab590cd
LuckyWind_sck <luckywindsck@gmail.com>
2019-11-05 14:55:04 +0900

    build: change rules of eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,11 +16,11 @@ module.exports = {
   },
   rules: {
     'func-names': 0,
-    'new-cap': 0,
-    'no-loop-func': 0,
-    'no-new': 0,
+    'no-console': 0,
     'no-param-reassign': 0,
     'no-plusplus': 0,
-    'no-restricted-syntax': 0,
+    'no-unused-expressions': ['error', {
+      allowShortCircuit: true,
+    }],
   },
 };
