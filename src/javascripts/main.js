b9516282a1161f881423d52c56b618bb4b82a507
LuckyWind_sck <luckywindsck@gmail.com>
2019-11-05 15:36:23 +0900

    feat: allow setting and add plugin

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,15 +1,18 @@
-/**
- * Comments are from laravel project
- */
-
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue.
- */
-
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import setting from '../config/setting.json';
+import Plugin from './plugins/plugin';
+
+// Globally import modules
+const modules = [
+  ['$setting', setting],
+];
+modules.forEach(([prop, value]) => {
+  Object.defineProperty(Vue.prototype, prop, { value });
+});
+
+// Use plugin
+Vue.use(Plugin);
 
 Vue.use(VueRouter);
 
@@ -19,7 +22,6 @@ const routes = [
   { path: '/foo', component: Foo },
   { path: '/bar', component: Bar },
 ];
-
 const router = new VueRouter({ routes });
 
 /**
@@ -40,12 +42,8 @@ files.keys().forEach((key) => {
   );
 });
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
+// Creating the Vue application instance
+// eslint-disable-next-line no-unused-vars
 const app = new Vue({
   router,
 }).$mount('.vue');
