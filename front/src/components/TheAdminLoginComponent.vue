da788b6218b15fc521dbfeac325a2481abd240b7
LuckyWind_sck <luckywindsck@gmail.com>
2019-12-09 13:42:15 +0900

    style: sort css properties

--- front/src/components/TheAdminLoginComponent.vue
+++ front/src/components/TheAdminLoginComponent.vue
@@ -50,98 +50,104 @@ export default {
 <style scoped>
 .admin-login {
   height: 100vh;
+
   background-color: #f9f9f9;
 
   font-family: BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif;
 }
 .form-login {
-  border-top: 1px solid #d8dee2;
-  border-radius: 5px;
   width: 50vw;
   margin: 0 auto;
+
+  border-top: 1px solid #d8dee2;
+  border-radius: 5px;
 }
 .header-login {
-  color: #333;
   text-align: center;
+
+  color: #333;
 }
 .title-login {
+  letter-spacing: -0.5px;
+
   font-size: 1.5em;
   font-weight: 300;
-  letter-spacing: -0.5px;
 }
 .body-login {
-  border-radius: 5px;
   padding: 20px;
-  font-size: 1em;
-  background-color: #fff;
+
   border: 1px solid #d8dee2;
+  border-radius: 5px;
+  background-color: #fff;
+
+  font-size: 1em;
 }
 .label-login {
   display: block;
+
   margin-bottom: 0.5em;
 }
 .text-input-login {
   display: block;
-  width: calc(100% - 2em);
 
-  padding: 0.5em 1em;
-  font-size: 1em;
-  font-weight: 600;
-  line-height: 1.25em;
+  width: calc(100% - 2em);
   margin-bottom: 1em;
+  padding: 0.5em 1em;
 
   vertical-align: middle;
 
-  outline: none;
   border: 1px solid #d1d5da;
   border-radius: 3px;
-  box-shadow: inset 0 1px 2px rgba(27, 31, 35, 0.075);
-
+  outline: none;
   background-color: #fff;
   background-repeat: no-repeat;
   background-position: right 8px center;
+  box-shadow: inset 0 1px 2px rgba(27, 31, 35, 0.075);
+
+  font-size: 1em;
+  font-weight: 600;
+  line-height: 1.25em;
 }
 .password-input-login {
   display: block;
-  width: calc(100% - 2em);
 
-  padding: 0.5em 1em;
-  font-size: 1em;
-  font-weight: 600;
-  line-height: 1.25em;
+  width: calc(100% - 2em);
   margin-bottom: 1em;
+  padding: 0.5em 1em;
 
   vertical-align: middle;
 
-  outline: none;
   border: 1px solid #d1d5da;
   border-radius: 3px;
-  box-shadow: inset 0 1px 2px rgba(27, 31, 35, 0.075);
-
+  outline: none;
   background-color: #fff;
   background-repeat: no-repeat;
   background-position: right 8px center;
-}
-.btn-login {
-  display: block;
-  width: 100%;
+  box-shadow: inset 0 1px 2px rgba(27, 31, 35, 0.075);
 
-  padding: 0.5em 1em;
   font-size: 1em;
   font-weight: 600;
   line-height: 1.25em;
+}
+.btn-login {
+  display: block;
+
+  width: 100%;
+  padding: 0.5em 1em;
 
   cursor: pointer;
   user-select: none;
-
   vertical-align: middle;
 
-  outline: none;
+  color: #fff;
   border: 1px solid rgba(27, 31, 35, 0.2);
   border-radius: 0.25em;
-
-  color: #fff;
+  outline: none;
   background-color: #28a745;
   background-image: linear-gradient(-180deg, #34d058, #28a745 90%);
+
+  font-size: 1em;
+  font-weight: 600;
+  line-height: 1.25em;
 }
 </style>
