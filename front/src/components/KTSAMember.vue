f4536e31024219754c424e0ca776c1d619f06609
LuckyWind_sck <luckywindsck@gmail.com>
2020-01-09 14:21:36 +0900

    feat: show members info

---
+++ front/src/components/KTSAMember.vue
@@ -0,0 +1,32 @@
+<template>
+  <figure class="member-figure">
+    <img class="about-us-image" :src="photoSrc" alt="" />
+    <figcaption>
+      <p>{{ role }}</p>
+      <p>{{ name }}</p>
+    </figcaption>
+  </figure>
+</template>
+
+<script>
+export default {
+  props: ['member'],
+  computed: {
+    role() {
+      return this.member ? this.member.role.join(' & ') : '';
+    },
+    name() {
+      return this.member ? [this.member.name, this.member.otherName].filter((e) => e).join(' / ') : '';
+    },
+    photoSrc() {
+      return this.member ? this.$images.MEMBERS[this.member.photoName || 'Default'] : '';
+    },
+  },
+};
+</script>
+
+<style>
+.member-figure {
+  text-align: center;
+}
+</style>
--- front/src/components/TheMembers.vue
+++ front/src/components/TheMembers.vue
@@ -1,13 +1,31 @@
 <template>
   <section class="ktsa-members">
     <div class="members-title">・MEMBERS</div>
-    <article class="members-content"></article>
+    <article class="members-content">
+      <div class="members-list" v-for="(row, r) in this.members" :key="r">
+        <div class="members-cell" v-for="(col, c) in row" :key="c">
+          <ktsa-member :member="col" />
+        </div>
+      </div>
+    </article>
   </section>
 </template>
 
 <script>
-export default {
+import settings from '@/config/settings';
 
+export default {
+  computed: {
+    members() {
+      const rows = Math.max(...settings.members.map(({ row }) => row)) + 1;
+      const cols = Math.max(...settings.members.map(({ col }) => col)) + 1;
+      const findMember = (member, row, col) => member.row === row && member.col === col;
+
+      return Array(rows).fill()
+        .map((_, row) => Array(cols).fill()
+          .map((_, col) => settings.members.find((member) => findMember(member, row, col))));
+    },
+  },
 };
 </script>
 
@@ -20,7 +38,13 @@ export default {
 .members-title {
   @mixin ktsa_page_title;
 }
-.about-us-content {
+.members-content {
   @mixin ktsa_page_content;
+}
+.members-list {
+  display: grid;
+
+  grid-template-columns: repeat(3, 1fr);
+  grid-column-gap: 1em;
 }
 </style>
