85baa72f796db12d08f879d565ca26e64e558852
LuckyWind_sck <luckywindsck@gmail.com>
2020-01-09 12:43:26 +0900

    feat: fill content

--- front/src/components/TheAboutUs.vue
+++ front/src/components/TheAboutUs.vue
@@ -1,3 +1,60 @@
 <template>
-  <div>About Us</div>
+  <section class="ktsa-about-us">
+    <div class="about-us-title">・ABOUT US</div>
+    <article class="about-us-content">
+      <figure class="about-us-figure">
+        <img
+          class="about-us-image"
+          :src="this.$images.ABOUTUS.members"
+          alt=""
+        />
+      </figure>
+      <h1>
+        團體概要
+      </h1>
+      <p>
+        慶應義塾大學台灣留學生會(Keio Taiwanese Student Association, KTSA)
+        是集結在慶應6個校區的台灣人所成立的學生組織。
+      </p>
+      <h1>
+        團體宗旨
+      </h1>
+      <p>
+        讓慶應台灣人交流認識、協助大家在日生活，並定期舉辦各種精彩活動讓會員能夠交到新朋友且更融入日本生活。
+      </p>
+    </article>
+  </section>
 </template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+.ktsa-about-us {
+  color: var(--KTSA-fg-blue);
+
+  @mixin ktsa_section;
+}
+.about-us-title {
+  padding: 1em 0 1em 1em;
+
+  border-color: black;
+  border-bottom: 0.2rem solid var(--KTSA-fg-blue);
+  background-color: var(--KTSA-bg-blue);
+
+  font-size: 1.5em;
+  font-weight: bold;
+}
+.about-us-content {
+  padding: 2em;
+}
+.about-us-figure {
+  width: 100%;
+}
+.about-us-image {
+  max-width: 100%;
+}
+</style>
