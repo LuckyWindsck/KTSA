aa8a6591e7475e92927d9bdadb7dae03bf35eed2
LuckyWind_sck <luckywindsck@gmail.com>
2020-01-15 18:16:12 +0900

    WIP

--- front/src/components/TheMain.vue
+++ front/src/components/TheMain.vue
@@ -3,9 +3,21 @@
     <div class="main-side">
       <the-nav class="navigation" />
       <div class="locales">
-        [ <span @click="changeLocale('zh-TW')">中文</span> /
-        <span @click="changeLocale('en')">English</span> /
-        <span @click="changeLocale('ja')">日文</span> ]
+        [
+        <span
+          :class="zhTW ? 'currentLocale' : ''"
+          @click="changeLocale('zh-TW')"
+          >中文</span
+        >
+        /
+        <span :class="en ? 'currentLocale' : ''" @click="changeLocale('en')"
+          >English</span
+        >
+        /
+        <span :class="ja ? 'currentLocale' : ''" @click="changeLocale('ja')"
+          >日文</span
+        >
+        ]
       </div>
     </div>
     <router-view class="main-router-view" />
@@ -14,6 +26,17 @@
 
 <script>
 export default {
+  computed: {
+    zhTW() {
+      return this.$store.state.i18n.locale === 'zh-TW';
+    },
+    en() {
+      return this.$store.state.i18n.locale === 'en';
+    },
+    ja() {
+      return this.$store.state.i18n.locale === 'ja';
+    },
+  },
   methods: {
     changeLocale(locale) {
       this.$store.commit('i18n/changeLocale', locale);
@@ -40,5 +63,8 @@ export default {
 }
 .main-router-view {
   grid-column: 2;
+}
+.currentLocale {
+  text-decoration: underline;
 }
 </style>
