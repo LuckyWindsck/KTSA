39f8a17255237ae177693d563d9703852cac8354
LuckyWind_sck <luckywindsck@gmail.com>
2020-01-16 13:16:31 +0900

    refactor: html & css

--- front/src/components/TheHomeSlideshow.vue
+++ front/src/components/TheHomeSlideshow.vue
@@ -1,11 +1,9 @@
 <template>
   <section class="slideshow">
     <div class="slide-container">
-      <div class="slide">
-        <img class="slide-image" :src="slideSrc" />
-      </div>
-      <a class="btn-prev-slide" @click="slideChange(-1)">&#10094;</a
-      ><a class="btn-next-slide" @click="slideChange(1)">&#10095;</a>
+      <div><img class="slide-image" :src="slideSrc" /></div>
+      <div class="btn-prev-slide" @click="slideChange(-1)">&#10094;</div>
+      <div class="btn-next-slide" @click="slideChange(1)">&#10095;</div>
     </div>
     <div class="slide-menu">
       <div class="slide-caption">{{ slideTitle }}</div>
@@ -99,9 +97,6 @@ export default {
 .slide-container {
   position: relative;
 }
-.slide {
-  width: 100%;
-}
 .slide-image {
   display: block;
 
@@ -112,23 +107,15 @@ export default {
   position: absolute;
   top: 50%;
 
-  width: auto;
   margin-top: -1em;
   padding: 1em;
 
   cursor: pointer;
   user-select: none;
   transition: 0.6s ease;
-
-  color: rgba(0, 0, 0, 0.8);
-
-  font-size: 1em;
-  font-weight: bold;
 }
 .btn-next-slide {
   right: 0;
-
-  border-radius: 3px 0 0 3px;
 }
 .btn-prev-slide:hover,
 .btn-next-slide:hover {
@@ -157,7 +144,6 @@ export default {
 
   width: 1rem;
   height: 1rem;
-  margin: 0 2px;
 
   cursor: pointer;
   transition: background-color 0.6s ease;
