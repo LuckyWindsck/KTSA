c32d23013a96602dd10daacdd8132e4fb16a68ae
LuckyWind_sck <luckywindsck@gmail.com>
2020-01-16 12:05:37 +0900

    refactor: move css of main to component & make locale component

--- front/src/components/TheIndex.vue
+++ front/src/components/TheIndex.vue
@@ -2,7 +2,7 @@
   <div class="background">
     <div class="layout">
       <the-header />
-      <the-main class="main" />
+      <the-main />
       <the-footer />
     </div>
   </div>
@@ -23,15 +23,5 @@ export default {
 .layout {
   max-width: 1000px;
   margin: 0 auto;
-}
-.main {
-  display: grid;
-
-  padding: 0.5rem;
-
-  background-color: var(--KTSA-bg-layout);
-
-  grid-template-columns: 3fr 9fr;
-  grid-column-gap: 1rem;
 }
 </style>
