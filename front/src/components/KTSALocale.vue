6f244555a0def446b9d90e104d5fbb4ec1dfd89d
LuckyWind_sck <luckywindsck@gmail.com>
2020-01-16 12:41:59 +0900

    refactor: maintain a locales dict instead of hardcode & use rem instead of em

--- front/src/components/KTSALocale.vue
+++ front/src/components/KTSALocale.vue
@@ -1,51 +1,46 @@
 <template>
   <div class="locales">
     [
-    <span :class="zhTW ? 'currentLocale' : ''" @click="changeLocale('zh-TW')"
-      >中文</span
-    >
-    /
-    <span :class="en ? 'currentLocale' : ''" @click="changeLocale('en')"
-      >English</span
-    >
-    /
-    <span :class="ja ? 'currentLocale' : ''" @click="changeLocale('ja')"
-      >日文</span
-    >
+    <span v-for="([code, name], i) in locales" :key="i">
+      <span :class="localesClass[code]" @click="changeLocale(code)">{{
+        name
+      }}</span>
+      <span>{{ i !== locales.length - 1 ? " / " : "" }}</span>
+    </span>
     ]
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      locales: Object.entries(this.$store.state.i18n.locales),
+    };
+  },
   computed: {
-    zhTW() {
-      return this.$store.state.i18n.locale === 'zh-TW';
-    },
-    en() {
-      return this.$store.state.i18n.locale === 'en';
-    },
-    ja() {
-      return this.$store.state.i18n.locale === 'ja';
+    localesClass() {
+      return Object.fromEntries(this.locales.map(([code, _]) => [
+        code,
+        this.$store.state.i18n.locale === code ? 'currentLocale' : '',
+      ]));
     },
   },
-
   methods: {
     changeLocale(locale) {
       this.$store.commit('i18n/changeLocale', locale);
     },
   },
-
 };
 </script>
 
 <style scoped>
 .locales {
-  margin: 1.5em auto;
+  margin: 1.5rem auto;
 
   text-align: center;
 
-  font-size: 1.5em;
+  font-size: 1.5rem;
 }
 .currentLocale {
   text-decoration: underline;
