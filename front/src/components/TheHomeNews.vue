300ce5260ec2280250baaac20ae44cb4f7bcd6c2
LuckyWind_sck <luckywindsck@gmail.com>
2020-01-16 13:21:52 +0900

    refactor: html & css

--- front/src/components/TheHomeNews.vue
+++ front/src/components/TheHomeNews.vue
@@ -3,14 +3,16 @@
     <div>
       <div class="home-news-title">・NEWS</div>
     </div>
-    <router-link
-      class="home-news-link"
-      v-for="(n, i) in news"
-      :key="i"
-      :to="{ name: 'news', params: { news_id: i } }"
-    >
-      <the-home-news-box class="home-news-box" :news="n" />
-    </router-link>
+    <div>
+      <router-link
+        class="home-news-link"
+        v-for="(n, i) in news"
+        :key="i"
+        :to="{ name: 'news', params: { news_id: i } }"
+      >
+        <the-home-news-box class="home-news-box" :news="n" />
+      </router-link>
+    </div>
   </section>
 </template>
 
@@ -34,12 +36,14 @@ export default {
   @mixin ktsa_section;
 }
 .home-news-title {
-  color: var(--KTSA-fg-blue);
   padding: 1em 0 1em 1em;
+
+  color: var(--KTSA-fg-blue);
   border-color: black;
   border-bottom: 0.2rem solid var(--KTSA-fg-blue);
   background-color: var(--KTSA-bg-layout);
-  font-size: 1.5em;
+
+  font-size: 1.5rem;
   font-weight: bold;
 }
 .home-news-box {
@@ -48,15 +52,15 @@ export default {
 .home-news-link {
   text-decoration: none;
 }
-.home-news-link:nth-of-type(1) .home-news-box {
+.home-news-link:nth-of-type(3n + 1) .home-news-box {
   color: var(--KTSA-fg-blue);
   background-color: var(--KTSA-bg-blue);
 }
-.home-news-link:nth-of-type(2) .home-news-box {
+.home-news-link:nth-of-type(3n + 2) .home-news-box {
   color: var(--KTSA-fg-red);
   background-color: var(--KTSA-bg-red);
 }
-.home-news-link:nth-of-type(3) .home-news-box {
+.home-news-link:nth-of-type(3n) .home-news-box {
   color: var(--KTSA-fg-yellow);
   background-color: var(--KTSA-bg-yellow);
 }
