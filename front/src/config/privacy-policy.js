68249b8ce7a319bf61ac27389dff3caac80a686d
LuckyWind_sck <luckywindsck@gmail.com>
2020-01-16 14:29:51 +0900

    feat: read privacy policy from file

---
+++ front/src/config/privacy-policy.js
@@ -0,0 +1,73 @@
+const privacyPolicy = {
+  'zh-TW': [
+    '非常歡迎您光臨「慶應義塾大學台灣留學生會網站」（以下簡稱本網站），為了讓您能夠安心使用本網站的各項服務與資訊，特此向您說明本網站的隱私權保護政策，以保障您的權益，請您詳閱下列內容：',
+    `一、隱私權保護政策的適用範圍
+隱私權保護政策內容，包括本網站如何處理在您使用網站服務時收集到的個人識別資料。隱私權保護政策不適用於本網站以外的相關連結網站，也不適用於非本網站所委託或參與管理的人員。`,
+    `二、個人資料的蒐集、處理及利用方式
+當您造訪本網站或使用本網站所提供之功能服務時，我們將視該服務功能性質，請您提供必要的個人資料，並在該特定目的範圍內處理及利用您的個人資料；非經您書面同意，本網站不會將個人資料用於其他用途。
+本網站在您使用服務信箱、問卷調查等互動性功能時，會保留您所提供的姓名、電子郵件地址、聯絡方式及使用時間等。
+於一般瀏覽時，伺服器會自行記錄相關行徑，包括您使用連線設備的IP位址、使用時間、使用的瀏覽器、瀏覽及點選資料記錄等，做為我們增進網站服務的參考依據，此記錄為內部應用，決不對外公佈。
+為提供精確的服務，我們會將收集的問卷調查內容進行統計與分析，分析結果之統計數據或說明文字呈現，除供內部研究外，我們會視需要公佈統計數據及說明文字，但不涉及特定個人之資料。`,
+    `三、資料之保護
+本網站主機均設有防火牆、防毒系統等相關的各項資訊安全設備及必要的安全防護措施，加以保護網站及您的個人資料採用嚴格的保護措施，只由經過授權的人員才能接觸您的個人資料，相關處理人員皆簽有保密合約，如有違反保密義務者，將會受到相關的法律處分。
+如因業務需要有必要委託其他單位提供服務時，本網站亦會嚴格要求其遵守保密義務，並且採取必要檢查程序以確定其將確實遵守。`,
+    `四、網站對外的相關連結
+本網站的網頁提供其他網站的網路連結，您也可經由本網站所提供的連結，點選進入其他網站。但該連結網站不適用本網站的隱私權保護政策，您必須參考該連結網站中的隱私權保護政策。`,
+    `五、與第三人共用個人資料之政策
+本網站絕不會提供、交換、出租或出售任何您的個人資料給其他個人、團體、私人企業或公務機關，但有法律依據或合約義務者，不在此限。
+
+前項但書之情形包括不限於：
+
+經由您書面同意。
+法律明文規定。
+為免除您生命、身體、自由或財產上之危險。
+與公務機關或學術研究機構合作，基於公共利益為統計或學術研究而有必要，且資料經過提供者處理或蒐集著依其揭露方式無從識別特定之當事人。
+當您在網站的行為，違反服務條款或可能損害或妨礙網站與其他使用者權益或導致任何人遭受損害時，經網站管理單位研析揭露您的個人資料是為了辨識、聯絡或採取法律行動所必要者。
+有利於您的權益。
+本網站委託廠商協助蒐集、處理或利用您的個人資料時，將對委外廠商或個人善盡監督管理之責。`,
+    `六、隱私權保護政策之修正
+本網站隱私權保護政策將因應需求隨時進行修正，修正後的條款將刊登於網站上。`,
+  ],
+  en: [
+    `Privacy Policy for Keio Taiwanese student association
+At Keio Taiwanese student association, accessible from https://keiotaiwan.org/home, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that are collected and recorded by Keio Taiwanese student association and how we use it.
+
+If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.`,
+    `Log Files
+Keio Taiwanese student association follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files includes internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.`,
+    `Privacy Policies
+You may consult this list to find the Privacy Policy for each of the advertising partners of Keio Taiwanese student association. Our Privacy Policy was created with the help of the Privacy Policy Generator and the Privacy Policy Generator Online.`,
+    `Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Keio Taiwanese student association, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.
+
+Note that the Keio Taiwanese student association has no access to or control over these cookies that are used by third-party advertisers.`,
+    `Third-Party Privacy Policies
+Keio Taiwanese student association's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. You may find a complete list of these Privacy Policies and their links here: Privacy Policy Links.
+Keio Taiwanese student association does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.`,
+    `Online Privacy Policy Only
+This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in the Keio Taiwanese student association. This policy is not applicable to any information collected offline or via channels other than this website.`,
+    `Consent
+By using our website, you hereby consent to our Privacy Policy and agree to its Terms and Conditions.`,
+  ],
+  ja: [
+    `Privacy Policy for Keio Taiwanese student association
+At Keio Taiwanese student association, accessible from https://keiotaiwan.org/home, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that are collected and recorded by Keio Taiwanese student association and how we use it.
+
+If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.`,
+    `Log Files
+Keio Taiwanese student association follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files includes internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.`,
+    `Privacy Policies
+You may consult this list to find the Privacy Policy for each of the advertising partners of Keio Taiwanese student association. Our Privacy Policy was created with the help of the Privacy Policy Generator and the Privacy Policy Generator Online.`,
+    `Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Keio Taiwanese student association, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.
+
+Note that the Keio Taiwanese student association has no access to or control over these cookies that are used by third-party advertisers.`,
+    `Third-Party Privacy Policies
+Keio Taiwanese student association's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. You may find a complete list of these Privacy Policies and their links here: Privacy Policy Links.
+Keio Taiwanese student association does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.`,
+    `Online Privacy Policy Only
+This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in the Keio Taiwanese student association. This policy is not applicable to any information collected offline or via channels other than this website.`,
+    `Consent
+By using our website, you hereby consent to our Privacy Policy and agree to its Terms and Conditions.`,
+  ],
+};
+
+export default privacyPolicy;
