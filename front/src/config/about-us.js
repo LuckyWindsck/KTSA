2157434cc3f43b165c0c5b683fd88843ce40941c
LuckyWind_sck <luckywindsck@gmail.com>
2020-01-16 14:36:13 +0900

    feat: read about us from file

---
+++ front/src/config/about-us.js
@@ -0,0 +1,17 @@
+const aboutUs = {
+  'zh-TW': {
+    團體概要: `慶應義塾大學台灣留學生會(Keio Taiwanese Student Association, KTSA)
+是集結在慶應6個校區的台灣人所成立的學生組織。`,
+    團體宗旨: '讓慶應台灣人交流認識、協助大家在日生活，並定期舉辦各種精彩活動讓會員能夠交到新朋友且更融入日本生活。',
+  },
+  en: {
+    INTRO: 'KTSA, Keio Taiwanese Student Association, is an association that gathers Taiwanese students enrolled in all 6 campuses of Keio University.',
+    PURPOSE: 'We dedicate to set up a platform for all Taiwanese students in Keio to meet each other and provide any help that we are capable of. A variety of interesting activities are also held regularly for everyone to enjoy! ',
+  },
+  ja: {
+    団体概要: '慶應義塾大学台湾留学生会（KTSA）は慶應大学の六つのキャンパスで在学している台湾人の学生を集めて成立した学生会である。',
+    成立の目的: '慶應大学の台湾人学生の交流、学生たちの生活支援などのために、定期的なイベントを毎年開催している。',
+  },
+};
+
+export default aboutUs;
